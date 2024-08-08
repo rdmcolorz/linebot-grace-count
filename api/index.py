743798a707f9bacd98fa7e4423d9d63fcd596edb
Taylor Yang <taylor.yang@redica.com>
2024-08-08 21:57:46 +0800

    add gsheet and flex message

--- api/index.py
+++ api/index.py
@@ -1,15 +1,46 @@
+import json
+from dotenv import load_dotenv
+import gspread
+from google.oauth2.service_account import Credentials
+
 from flask import Flask, request, abort
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, BubbleContainer, BoxComponent, TextComponent, ButtonComponent, URIAction
+
 
 import os
+
+load_dotenv()
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+cred_json = json.loads(os.getenv("SERVICE_ACC_SECRET"))
+
+creds = Credentials.from_service_account_info(cred_json, scopes=scope)
+client = gspread.authorize(creds)
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
+
+def create_flex_message(type):
+    bubble = BubbleContainer(
+        direction='ltr',
+        body=BoxComponent(
+            layout='vertical',
+            contents=[
+                TextComponent(text=type, weight='bold', size='xl'),
+                ButtonComponent(
+                    action=URIAction(label='Go to Website', uri='https://google.com')
+                )
+            ]
+        )
+    )
+    flex_message = FlexSendMessage(alt_text='This is a Flex Message', contents=bubble)
+    return flex_message
 
 # domain root
 @app.route('/')
@@ -41,7 +72,8 @@ def handle_message(event):
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+            create_flex_message("prayer_meeting")
+        )
         return
 
     # if working_status:
@@ -54,4 +86,7 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
+    # spreadsheet = client.open_by_key('1wMN8njXEchf9-GedPcsz0eKCvJpYUBxaHPUelBdamKQ')
+    # sheet = spreadsheet.worksheet('test')
+    # sheet.update('A1:B2', [[1, 2], [3, 4]])
     app.run()
